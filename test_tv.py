f3b2f5ac00768d750da0b3ae12cb54c5a2c406f8
Katrina Pabuna <kinapabuna@gmail.com>
2023-05-13 20:33:53 +0800

    Create second object

--- test_tv.py
+++ test_tv.py
@@ -4,15 +4,28 @@
 import tv_class
 
 # Create first object of class TV
-tv1 = tv_class.TV(30, 2, True, False)
+tv1 = tv_class.TV(30, 3, True, False)
 
 # Create second object of class TV
+tv2 = tv_class.TV(3, 2, True, False)
 
-# call instance methods and parameters
+# Call instance methods and parameters
+# TV 1
+print("\nTV 1")
 print("Channel:", tv1.get_channel())
 print("Volume level:", tv1.get_volume_level())
 
 if tv1.turn_on() == True:
-    print("The TV is on.")
+    print("The TV is on.\n")
 else:
-    print("The TV is off.")
+    print("The TV is off.\n")
+
+# TV 2
+print("TV 2")
+print("Channel:", tv2.get_channel())
+print("Volume level:", tv2.get_volume_level())
+
+if tv2.turn_on() == True:
+    print("The TV is on.\n")
+else:
+    print("The TV is off.\n")
