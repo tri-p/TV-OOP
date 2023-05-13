b0341c135e7e1b2eacecf246e9ca54737dc97717
Katrina Pabuna <kinapabuna@gmail.com>
2023-05-13 20:25:03 +0800

    Get the rest of the parameters

--- test_tv.py
+++ test_tv.py
@@ -4,9 +4,15 @@
 import tv_class
 
 # Create first object of class TV
-tv1 = tv_class.TV(30, 2, True)
+tv1 = tv_class.TV(30, 2, True, False)
 
 # Create second object of class TV
 
 # call instance methods and parameters
-print(tv1.get_channel())
+print("Channel:", tv1.get_channel())
+print("Volume level:", tv1.get_volume_level())
+
+if tv1.turn_on() == True:
+    print("The TV is on.")
+else:
+    print("The TV is off.")
--- tv_class.py
+++ tv_class.py
@@ -3,16 +3,27 @@
 # Create class TV
 class TV:
     # constructor
-    def __init__(self, channel, volume_level, on):
+    def __init__(self, channel, volume_level, on, off):
         '''Create a TV instance where
         channel = int(channel of TV)
         volume_level = int(volume level of TV)
-        on = bool(if the TV is on or not)'''
+        on = bool(if the TV is on or not)
+        off = bool(if the TV is on or not)'''
 
         # instance variables
         self.channel = channel
         self.volume_level = volume_level
         self.on = on
+        self.off = off
     
     def get_channel(self):
         return self.channel
+
+    def get_volume_level(self):
+        return self.volume_level
+    
+    def turn_on(self):
+        return self.on
+    
+    def turn_off(self):
+        return self.off
