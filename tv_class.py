7669a4dccbc4181c2b254d22d3ecbece4f119a11
Katrina Pabuna <kinapabuna@gmail.com>
2023-05-16 17:15:43 +0800

    Create a function to turn on TV 1 or TV 2

--- tv_class.py
+++ tv_class.py
@@ -19,3 +19,21 @@ class TV:
 tv1 = TV(30, 3, True, False)
 tv2 = TV(3, 2, True, False)
 
+# Create a function that lets the user to choose between TV1 and TV2
+def on_tv():
+    '''Create def on_tv where it asks the user
+    to turn on either TV 1 or TV 2.'''
+    try:
+        user_input = int(input("\nWhat TV would you like to turn on?\n" +
+                                "1. TV 1\n" +
+                                "2. TV 2\n\n"))
+        if user_input == 1:
+            print("") # should lead to TV 1
+        elif user_input == 2:
+            print("") # should lead to TV 2
+        else:
+            print("Invalid input.")
+    except ValueError:
+        print("Only integers are allowed.")
+
+on_tv()
